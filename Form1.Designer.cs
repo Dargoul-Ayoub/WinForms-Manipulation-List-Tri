59c748237ca48d08283d67f0ec788511fc660fcf
infoayoub <infoayoub@github.com>
2021-02-25 23:05:19 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Manipulation_List_Tri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(44, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(163, 199);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(573, 62);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(187, 212);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(349, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(349, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(349, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(36, 27);
+            this.button3.TabIndex = 5;
+            this.button3.Text = ">>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(349, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "<<";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(656, 311);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(26, 27);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "↓";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(718, 311);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(26, 27);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "↑";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(716, 367);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(44, 27);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Z-A";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.AutoSize = true;
+            this.button8.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(640, 367);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(44, 27);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "A-Z";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.AutoSize = true;
+            this.button9.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(62, 311);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(26, 27);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "↓";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.AutoSize = true;
+            this.button10.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(120, 311);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(26, 27);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "↑";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.AutoSize = true;
+            this.button11.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(44, 367);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(44, 27);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "A-Z";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.AutoSize = true;
+            this.button12.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(120, 367);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(44, 27);
+            this.button12.TabIndex = 14;
+            this.button12.Text = "Z-A";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manipulation_List_Tri
+{
+    public partial class Form1 : Form
+    {
+        // List<string> ListBox_1 = new List<string>() { "Epluche dsquette", "Presse à électrons", "A", "B", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n","o","p","q","r","s","t","w" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                int index = listBox1.SelectedIndex + 2;
+                listBox2.Items.Insert(index, listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+                listBox2.SelectedIndex = index - 1;
+            }
+            listBox2.Sorted = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != 0 && listBox1.SelectedIndex != listBox1.Items.Count - 1)
+            {
+
+                button10.Enabled = true;
+                button9.Enabled = true;
+            }
+            else if (listBox1.SelectedIndex == 0 && listBox1.SelectedIndex != listBox1.Items.Count - 1)
+            {
+                button10.Enabled = false;
+                button9.Enabled = true;
+
+            }
+            else if (listBox1.SelectedIndex == listBox1.Items.Count - 1 && listBox1.SelectedIndex != 0)
+            {
+                button10.Enabled = true;
+                button9.Enabled = false;
+            }
+            else
+            {
+                button10.Enabled = false;
+                button9.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // listBox1.DataSource = ListBox_1;
+            listBox1.Items.Add("ayoub");
+            listBox1.Items.Add("aze");
+            listBox1.Items.Add("a");
+            listBox1.Items.Add("b");
+            listBox1.Items.Add("c");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* if  if (listBox1.SelectedItem != null && listBox1.SelectedIndex != 0 || listBox1.SelectedItem != null && listBox1.SelectedIndex != listBox1.Items.Count - 1)
+              {
+                  listBox2.Items.Add(listBox1.SelectedItem);
+                  ListBox_1.Remove(Convert.ToString(listBox1.SelectedItem));
+                  listBox1.DataSource = null;
+                  listBox1.DataSource = ListBox_1;
+                  button10.Enabled = false;
+                  button9.Enabled = false;
+              }
+              else if (listBox1.SelectedIndex == 0)
+              {
+                  listBox2.Items.Add(listBox1.SelectedItem);
+                  ListBox_1.Remove(Convert.ToString(listBox1.SelectedItem));
+                  listBox1.DataSource = null;
+                  listBox1.DataSource = ListBox_1;
+                  button10.Enabled = false;
+                  button9.Enabled = true;
+
+              }
+              else if (listBox1.SelectedIndex == listBox1.Items.Count - 1)
+              {
+                  listBox2.Items.Add(listBox1.SelectedItem);
+                  ListBox_1.Remove(Convert.ToString(listBox1.SelectedItem));
+                  listBox1.DataSource = null;
+                  listBox1.DataSource = ListBox_1;
+                  button10.Enabled = true;
+                  button9.Enabled = false;
+              }    */
+            if (listBox1.SelectedItem != null)
+            {
+                listBox2.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                listBox1.Focus(); // it's not working
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*  if (listBox2.SelectedItem != null)
+              {
+                  ListBox_1.Add(Convert.ToString(listBox2.SelectedItem));
+                  listBox1.DataSource = null;
+                  listBox1.DataSource = ListBox_1;
+                  listBox2.Items.Remove(listBox2.SelectedItem);              
+             }*/
+
+            if (listBox2.SelectedItem != null)
+            {
+                listBox1.Items.Add(listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+                listBox2.Focus();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            /*
+            string selcted = Convert.ToString(listBox1.SelectedItem);
+
+            ListBox_1.Insert(listBox1.SelectedIndex+2, Convert.ToString(listBox1.SelectedItem));
+            ListBox_1.Remove(Convert.ToString(listBox1.SelectedItem));
+            listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+            listBox1.DataSource = null;
+            listBox1.DataSource = ListBox_1;
+            */
+            if (listBox1.SelectedItem != null)
+            {
+                int index = listBox1.SelectedIndex + 2;
+                listBox1.Items.Insert(index, listBox1.SelectedItem);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                listBox1.SelectedIndex = index - 1;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                int index = listBox1.SelectedIndex - 1;
+                listBox1.Items.Insert(index, listBox1.SelectedItem);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                listBox1.SelectedIndex = index;
+            }
+            listBox1.Sorted = false;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                int index = listBox2.SelectedIndex - 1;
+                listBox2.Items.Insert(index, listBox2.SelectedItem);
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+                listBox2.SelectedIndex = index;
+            }
+            listBox2.Sorted = false;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != 0 && listBox2.SelectedIndex != listBox2.Items.Count - 1)
+            {
+
+                button6.Enabled = true;
+                button5.Enabled = true;
+            }
+            else if (listBox2.SelectedIndex == 0 && listBox2.SelectedIndex != listBox2.Items.Count - 1)
+            {
+                button6.Enabled = false;
+                button5.Enabled = true;
+
+            }
+            else if (listBox2.SelectedIndex == listBox2.Items.Count - 1 && listBox2.SelectedIndex != 0)
+            {
+
+                button6.Enabled = true;
+                button5.Enabled = false;
+            }
+            else
+            {
+                button5.Enabled = false;
+                button6.Enabled = false;
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.AddRange(listBox1.Items);
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.AddRange(listBox2.Items);
+            listBox2.Items.Clear();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Sorted = true;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true;
+            for (int i = 0; i < listBox1.Items.Count / 2; i++)
+            {
+                string mot = Convert.ToString(listBox1.Items[i]);
+                listBox1.Items[i] = listBox1.Items[listBox1.Items.Count - 1 - i];
+                listBox1.Items[listBox1.Items.Count - 1 - i] = mot;
+            }
+            listBox1.Sorted = false;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox2.Sorted = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox2.Sorted = true;
+            for (int i = 0; i < listBox2.Items.Count / 2; i++)
+            {
+                string mot = Convert.ToString(listBox2.Items[i]);
+                listBox2.Items[i] = listBox2.Items[listBox2.Items.Count - 1 - i];
+                listBox2.Items[listBox2.Items.Count - 1 - i] = mot;
+            }
+            listBox2.Sorted = false;
+        }
+    }
+}
